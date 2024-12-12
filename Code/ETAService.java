bbb58d22aaf33e4e0c5f658145eedae4a39c8e64
John-E-Boy <116788228+John-E-Boy@users.noreply.github.com>
2024-12-11 23:07:45 -0800

    Delete Code directory

--- Code/ETAService.java
+++
@@ -1,9 +0,0 @@
-public class ETAService {
-
-    public double avgWalkingSpeed = 2.6; // mph
-    public double avgSpeedDisabled = 1.5; // mph
-    public TimeSpan calculateETA(Route route) {
-        // Calculation logic for ETA based on route distance and speed assumptions
-        return route.getEstimatedTime();
-    }
-}
